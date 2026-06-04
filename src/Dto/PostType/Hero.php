9e17fd66cb26fe5d7de06802937beb48ddbcdd3c
vigihdev <vigihdev@email.com>
2026-06-04 15:55:38 +0700

    ✨ 4-6-2026 15-20

---
+++ src/Dto/PostType/Hero.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ThrubusWP\Widgets\Dto\PostType;
+
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+final class Hero
+{
+
+    public function __construct(
+        public readonly string $title,
+        #[SerializedName('image_url')]
+        public readonly string $imageUrl,
+        public readonly string $description,
+        public readonly ?string $subtitle = null,
+    ) {}
+}
