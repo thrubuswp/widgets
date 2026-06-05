e2b0d0e35d67f4e72783f983ec78d03abf01b0b9
vigihdev <vigihdev@email.com>
2026-06-05 16:01:29 +0700

    💥 2026-06-05 16:01:29

---
+++ src/PostType/Dto/ServiceEkspedisiDto.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ThrubusWP\Widgets\PostType\Dto;
+
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+final class ServiceEkspedisiDto
+{
+
+    public function __construct(
+        public readonly string $title,
+        public readonly string $description,
+        #[SerializedName('image_url')]
+        public readonly string $imageUrl,
+    ) {}
+}
